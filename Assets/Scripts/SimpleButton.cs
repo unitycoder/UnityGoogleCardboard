96d83da1db6d1356f6ec1631fcd432d75ddf261f
mika <unitycoder@users.noreply.github.com>
2021-10-12 15:56:58 +0300

    init 2019.4.25f1

---
+++ Assets/Scripts/SimpleButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SimpleButton : MonoBehaviour
+{
+    public UnityEvent OnPointerClicked;
+
+    /// <summary>
+    /// This method is called by the Main Camera when it starts gazing at this GameObject.
+    /// </summary>
+    public void OnPointerEnter()
+    {
+        var mat = GetComponent<Image>().color = Color.red;
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it stops gazing at this GameObject.
+    /// </summary>
+    public void OnPointerExit()
+    {
+        var mat = GetComponent<Image>().color = Color.white;
+
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it is gazing at this GameObject and the screen
+    /// is touched.
+    /// </summary>
+    public void OnPointerClick()
+    {
+        OnPointerClicked?.Invoke();
+    }
+}
